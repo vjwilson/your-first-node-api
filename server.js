18921e5b244c67f2205b579d36c43731f6ee6136
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2017-04-05 17:43:52 -0400

    Add GET endpoint for all recipes

--- server.js
+++ server.js
@@ -3,6 +3,38 @@ let app = express();
 
 let port = 7777;
 
+let recipes = [
+  {
+    "id": 1,
+    "name": "Pecan Pie",
+    "author": "Mariah Reynolds",
+    "ingredients": [
+      "3 eggs",
+      "1 c. white sugar",
+      "dash salt",
+      "1 tsp. vanilla",
+      "1/2 c. dark Karo syrup"
+    ],
+    "directions": "Melt 6 Tbsp. butter, mix in. Add 1 c. chopped nuts. Bake 57 mins. at 325 or 350."
+  },
+  {
+    "id": 2,
+    "name": "Pound Cake",
+    "author": "Eliza Hamilton",
+    "ingredients": [
+      "1 lb. cake flour",
+      "1 lb. sugar",
+      "1 lb. butter",
+      "1 egg"
+    ],
+    "directions": "Mix all ingredients thoroughly. Pour into cake pan. Bake at 350 for 1 hour and 20 minutes."
+  }
+];
+
+app.get('/recipes', function (req, res) {
+  res.json(recipes);
+});
+
 app.get('/', function (req, res) {
   res.send('Welcome to the recipe API.');
 });
